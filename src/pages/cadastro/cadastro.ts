c0c11aedd118fe5554701ba0be00f53af63eb2c3
washington.silva <washington.silva@totvs.com.br>
2017-10-13 19:11:48 -0300

    #cadastro

--- src/pages/cadastro/cadastro.ts
+++ src/pages/cadastro/cadastro.ts
@@ -11,9 +11,21 @@ export class CadastroPage {
   public sala: Sala;
   public precoTotal: number;
 
+  public nome: string;
+  public endereco: string;
+  public email: string;
+  public data: string = new Date().toISOString();
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
     this.sala = navParams.get('carro');
     this.precoTotal = navParams.get('precoTotal');
+  }
+
+  agenda(){
+    console.log(this.nome);
+    console.log(this.endereco);
+    console.log(this.email);
+    console.log(this.data);
   }
 
 }
